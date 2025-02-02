334cef4de7990266a9bac17b0f3ac2a5ca028e6d
Andrei Karepin <karepinandrei@gmail.com>
2025-02-02 20:57:29 +0200

    starting render

--- cron.php
+++ cron.php
@@ -11,13 +11,18 @@ function render_childs($zoom,$x,$y){
 	OverlayRenderer::handle($zoom+1,$x*2+1,$y*2);
 	OverlayRenderer::handle($zoom+1,$x*2+1,$y*2+1);
 }
-//OverlayRenderer::handle(4,8,5);//голубой банан
-//OverlayRenderer::handle(3,4,2);//главная вырезка голубого банана//2.2.1//1.1.0
+OverlayRenderer::handle(6,32,20);
+die();
 
-//OverlayRenderer::handle(1,0,0);
-//OverlayRenderer::handle(1,0,1);
-//OverlayRenderer::handle(1,1,0);
-//OverlayRenderer::handle(1,1,1);
+
+
+OverlayRenderer::handle(4,8,5);//голубой банан
+OverlayRenderer::handle(3,4,2);//главная вырезка голубого банана//2.2.1//1.1.0
+
+OverlayRenderer::handle(1,0,0);
+OverlayRenderer::handle(1,0,1);
+OverlayRenderer::handle(1,1,0);
+OverlayRenderer::handle(1,1,1);
 
 for($i=6;$i<11;$i++){
 	$files=glob(__DIR__.'/lb_json/l_'.$i.'*.packed');
